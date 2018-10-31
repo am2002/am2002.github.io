8e927a029ccce980c298973133aa0305f1203fcd
am2002 <44576253+am2002@users.noreply.github.com>
2018-10-31 22:51:21 +0100

    BIG

--- Script/index-Script.js
+++ Script/index-Script.js
@@ -1,4 +1,25 @@
-function indexToContact()
+function ToIndex()
+{
+    window.location ="loading.html";
+}
+
+
+function ToVakken()
+{
+    window.location="vakken.html";
+}
+
+function ToLeraren()
+{
+    window.location ="leraren.html";
+}
+
+function ToAuteurs()
+{
+    window.location ="auteurs.html";
+}
+
+function ToContact()
 {
     window.location="contact.html";
 }
---
+++ Script/vakken-Script.js
@@ -0,0 +1,25 @@
+function ToIndex()
+{
+    window.location ="loading.html";
+}
+
+
+function ToVakken()
+{
+    window.location="vakken.html";
+}
+
+function ToLeraren()
+{
+    window.location ="leraren.html";
+}
+
+function ToAuteurs()
+{
+    window.location ="auteurs.html";
+}
+
+function ToContact()
+{
+    window.location="contact.html";
+}
